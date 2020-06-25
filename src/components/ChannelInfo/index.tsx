aa9856aa1472ff1ccefec07a8881220b1f75b8d6
Anderson0017 <andeerson0017@gmail.com>
2020-06-25 19:30:51 -0300

    create component Channel Info

---
+++ src/components/ChannelInfo/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Container, Title, HashtagIcon, Separator, Description } from './styles';
+
+const ChannelInfo: React.FC = () => {
+  return (
+    <Container>
+      <HashtagIcon/>
+
+      <Title>Chat livre</Title>
+      <Separator/>
+
+      <Description>Canal Aberto para conversas</Description>
+    </Container>
+  );
+}
+
+export default ChannelInfo;
