6eee5c8b8f4e848543cfa69c6fe1e11bac4989b0
Anderson0017 <andeerson0017@gmail.com>
2020-06-25 19:47:52 -0300

    create component Channel List

--- src/components/ChannelList/index.tsx
+++ src/components/ChannelList/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Category, AddCategoryIcon, /* ChannelButton */ } from './styles';
 
 const ChannelList: React.FC = () => {
   return (
     <Container>
- 
+      <Category>
+        <span>Canais de texto</span>
+        <AddCategoryIcon/>
+      </Category>
+
+      {/*  <ChannelButton channel-name="chat-livre"/>
+        <ChannelButton channel-name="chat-fechado"/>
+        <ChannelButton channel-name="chat-parcialmente fechado"/>
+        <ChannelButton channel-name="chat-totalmente fechado"/> */}
     </Container>
   );
 }
