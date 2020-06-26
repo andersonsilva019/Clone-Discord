fc33bbbd75c567282df5aa056b9000249b64b66e
Anderson0017 <andeerson0017@gmail.com>
2020-06-25 21:15:30 -0300

    create component User List

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Role, User, Avatar } from './styles';
+
+interface UserProps {
+  nickname: string; 
+  isBot?: boolean;
+}
+
+const UserRow: React.FC<UserProps> = ({ nickname, isBot }) => {
+  return (
+    <User>
+      <Avatar className={isBot ? 'bot' : ''}/>
+      <strong>{nickname}</strong>
+      {isBot && <span>Bot</span>}
+    </User>
+  );
+}
 
 const UserList: React.FC = () => {
   return (
     <Container>
-      
+      <Role>Disponível - 1</Role>
+      <UserRow nickname="Anderson Silva Souza teste react"/>
+
+      <Role>Offline - 18</Role>
+      <UserRow nickname="Anderson " isBot/>
+
+
     </Container>
   );
 }
